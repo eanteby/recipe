7a584f32689ae72dfaba660a21a179ee611a1d94
eanteby <eanteby2@gmail.com>
2025-01-06 22:51:03 -0500

    recipe updates 2-15

--- RecipeApps/RecipeWinforms/frmRecipeSearch.cs
+++ RecipeApps/RecipeWinforms/frmRecipeSearch.cs
@@ -1,4 +1,5 @@
 ﻿using CPUFramework;
+using CPUWindowsFormsFramework;
 using System.Configuration;
 using System.Data;
 
@@ -10,14 +11,14 @@ namespace RecipeWinforms
         {
             InitializeComponent();
             btnSearch.Click += BtnSearch_Click;
-            FormatGrid();
+            btnNew.Click += BtnNew_Click;
+            WindowsFormsUtility.FormatGridForSearchResults(gRecipe);
             gRecipe.CellDoubleClick += gRecipe_CellDoubleClick;
         }
 
-
         private void SearchForRecipe(string lastname)
         {
-            string sql = "select * from recipe r join heartyhearthuser h on r.heartyhearthuserid = h.heartyhearthuserid join cuisine c on r.cuisineid = c.cuisineid where r.recipename like '%" + txtRecipeName.Text + "%'";
+            string sql = "select r.RecipeId, r.RecipeName, h.UserName, r.NumCalories, c.CuisineType, r.DateDrafted, r.DatePublished, r.DateArchived, r.RecipeStatus, r.RecipePic from recipe r join heartyhearthuser h on r.heartyhearthuserid = h.heartyhearthuserid join cuisine c on r.cuisineid = c.cuisineid where r.recipename like '%" + txtRecipeName.Text + "%'";
             DataTable dt = SQLUtility.GetDataTable(sql);
             gRecipe.DataSource = dt;
             gRecipe.Columns["RecipeId"].Visible = false;
@@ -25,17 +26,13 @@ namespace RecipeWinforms
 
         private void ShowRecipeForm(int rowindex)
         {
-            int id = (int)gRecipe.Rows[rowindex].Cells["RecipeId"].Value;
+            int id = 0;
+            if (rowindex > -1)
+            {
+                id = (int)gRecipe.Rows[rowindex].Cells["RecipeId"].Value;
+            }
             frmRecipe frm = new frmRecipe();
             frm.ShowForm(id);
-        }
-
-        private void FormatGrid()
-        {
-            gRecipe.AllowUserToAddRows = false;
-            gRecipe.ReadOnly = true;
-            gRecipe.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
-            gRecipe.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
         }
 
         private void gRecipe_CellDoubleClick(object? sender, DataGridViewCellEventArgs e)
@@ -46,6 +43,11 @@ namespace RecipeWinforms
         private void BtnSearch_Click(object? sender, EventArgs e)
         {
             SearchForRecipe(lblRecipe.Text);
+        }
+
+        private void BtnNew_Click(object? sender, EventArgs e)
+        {
+            ShowRecipeForm(-1);
         }
     }
 }
