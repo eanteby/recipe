f78c37cd2bd50b6013b7421495e5293601a0333b
eanteby <eanteby2@gmail.com>
2025-01-20 17:35:58 -0500

    HW change

--- Database Files/sprocs/HeartyHearthUserGet.sql
+++ Database Files/sprocs/HeartyHearthUserGet.sql
@@ -1,11 +1,11 @@
-create or alter procedure dbo.HeartyHearthUserGet(@HeartyHearthUserId int = 0, @All bit = 0, @UserLastName varchar(30) = '')
+create or alter procedure dbo.HeartyHearthUserGet(@HeartyHearthUserId int = 0, @All bit = 0, @UserName varchar(30) = '')
 as
 begin
 	select h.HeartyHearthUserId, h.UserFirstName, h.UserLastName, h.UserName
 	from HeartyHearthUser h 
 	where h.HeartyHearthUserId = @HeartyHearthUserId
 	or @All = 1
-	or (@UserLastName <> '' and h.UserLastName like '%' + @UserLastName + '%')
+	or (@UserName <> '' and h.UserName like '%' + @UserName + '%')
 end
 go
 
