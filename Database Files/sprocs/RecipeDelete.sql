7e36ec3fce72574a74c6c136f1864a6a445902fd
Esther Anteby <eanteby2@gmail.com>
2025-02-25 21:07:57 -0500

    RecipeDelete sproc update

--- Database Files/sprocs/RecipeDelete.sql
+++ Database Files/sprocs/RecipeDelete.sql
@@ -1,6 +1,13 @@
-create or alter procedure dbo.RecipeDelete(@RecipeId int)
+create or alter procedure dbo.RecipeDelete(@RecipeId int, @message varchar(500) = '' output)
 as
 begin
+	declare @return int = 0
+	if exists(select * from recipe r where (r.RecipeStatus = 'published' or (r.RecipeStatus = 'archived' and datediff(day, r.DateArchived, getdate()) <30)) and r.RecipeId = @RecipeId)
+	begin
+		select @return = 1, @message = 'cannot delete recipe unless drafted or archived for 30+ days'
+		goto finished
+	end
+
 	begin try
 		begin tran
 		delete RecipeIngredient where RecipeId =@RecipeId
@@ -12,5 +19,11 @@ begin
 		rollback;
 		throw
 	end catch
+
+	finished:
+	return @return
 end
 go
+
+
+select * from recipe
