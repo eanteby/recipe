f011bfcddb1b4a29950e1746e852ff954e31b5f8
Esther Anteby <eanteby2@gmail.com>
2025-03-02 17:00:10 -0500

    RecipeUpdate sproc

---
+++ Database Files/sprocs/RecipeUpdate.sql
@@ -0,0 +1,37 @@
+create or alter proc dbo.RecipeUpdate(
+@RecipeId int  output,
+@HeartyHearthUserId int ,
+@CuisineId int ,
+@RecipeName varchar (150),
+@NumCalories int ,
+@DateDrafted datetime ,
+@DatePublished datetime ,
+@DateArchived datetime ,
+@Message varchar(500) = '' output
+)
+as
+begin
+	declare @return int = 0
+	select @RecipeId = isnull(@RecipeId, 0), @HeartyHearthUserId = isnull(@HeartyHearthUserId, 0)
+
+	if(@RecipeId = 0)
+	begin 
+		insert Recipe(CuisineId, HeartyHearthUserId, RecipeName, NumCalories, DateDrafted, DatePublished, DateArchived)
+		values(@CuisineId, @HeartyHearthUserId, @RecipeName, @NumCalories, '', '' , '')
+
+		select @RecipeId = scope_identity()
+	end
+	else
+	begin
+		update recipe
+		set
+		CuisineId = @CuisineId, 
+		RecipeName = @RecipeName, 
+		NumCalories = @NumCalories,
+		DateDrafted = @DateDrafted, 
+		DatePublished = @DatePublished, 
+		DateArchived = @DateArchived
+		where RecipeId = @RecipeId
+
+	end
+end
